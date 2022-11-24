894eccca73a64850a91df90b0239201c54cd23a8
Mehmet Tekin <mehmet.tekin@femank.local>
2022-11-25 00:11:29 +0300

    Cutomer validator yazıldı.

--- Project.FluentValidation/Controllers/CustomersController.cs
+++ Project.FluentValidation/Controllers/CustomersController.cs
@@ -53,7 +53,7 @@ namespace Project.FluentValidation.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Email,Age")] Customer customer)
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Age,BirthDay")] Customer customer)
         {
             if (ModelState.IsValid)
             {
--- Project.FluentValidation/FluentValidator/CustomerValidator.cs
+++ Project.FluentValidation/FluentValidator/CustomerValidator.cs
@@ -5,11 +5,16 @@ namespace Project.FluentValidation.FluentValidator
 {
     public class CustomerValidator:AbstractValidator<Customer>
     {
+        public string EmptyMessage { get; } = "{PropertyName} alanı boş olamaz.";
         public CustomerValidator()
         {
             RuleFor(X=>X.Name).NotEmpty().WithMessage("İsim alanı boş geçilemez."); //Boş geçilemez olsun.
             RuleFor(X=>X.Email).NotEmpty().WithMessage("Mail alanı boş geçilemez.").EmailAddress().WithMessage("Mail formatı düzgün biçimde değil."); //Önce boş geçilemez kontrolü daha sonra da mail tip kontrolü yaptık abc@mail.com şeklinde bir alan olamlı dedik aslında
             RuleFor(x => x.Age).NotEmpty().WithMessage("Yaş alanı boş geçilemez.").InclusiveBetween(18, 60).WithMessage("Yaş aralığı 18 ile 60 arasında olmalıdır.");
+            RuleFor(x => x.BirthDay).NotEmpty().WithMessage(EmptyMessage).Must(x =>
+            {
+                return DateTime.Now.AddYears(-18)>=x;
+            }).WithMessage("Yaşınız18 yaşından büyük olamlıdır.");
         }
     }
 }
