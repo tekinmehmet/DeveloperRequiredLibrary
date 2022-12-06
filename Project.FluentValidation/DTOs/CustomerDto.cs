1b0a1be77c196ca590ab89e9ca9fb6d4b4b3f7e3
mehmet.tekin <tekimmehmet@hotmail.com>
2022-12-06 13:39:09 +0300

    IncludeMembers metodu

--- Project.FluentValidation/DTOs/CustomerDto.cs
+++ Project.FluentValidation/DTOs/CustomerDto.cs
@@ -7,7 +7,7 @@
         public string? Eposta { get; set; }
         public int Yas { get; set; }
         public string FullName { get; set; }
-        public string CreditCardNumber { get; set; }
-        public DateTime CreditCardValidDate { get; set; }
+        public string Number { get; set; }
+        public DateTime ValidDate { get; set; }
     }
 }
--- Project.FluentValidation/Mapping/CustomerProfile.cs
+++ Project.FluentValidation/Mapping/CustomerProfile.cs
@@ -8,11 +8,15 @@ namespace Project.FluentValidation.Mapping
     {
         public CustomerProfile()
         {
+            CreateMap<CreditCard, CustomerDto>();
             CreateMap<Customer, CustomerDto>()
+                .IncludeMembers(x=>x.CreditCard)
                 .ForMember(dest => dest.Isım, opt => opt.MapFrom(x => x.Name))
                 .ForMember(dest => dest.Eposta, opt => opt.MapFrom(x => x.Email))
                 .ForMember(dest => dest.Yas, opt => opt.MapFrom(x => x.Age))
-                .ForMember(dest=>dest.FullName,opt=>opt.MapFrom(x=>x.FerreAnkastre()));
+                .ForMember(dest => dest.FullName, opt => opt.MapFrom(x => x.FerreAnkastre()));
+                //.ForMember(dest=>dest.CCNumber,opt=>opt.MapFrom(x=>x.CreditCard.Number))
+                //.ForMember(dest => dest.CCValidDate, opt => opt.MapFrom(x => x.CreditCard.ValidDate));
             //CreateMap<Customer, CustomerDto>().ReverseMap();
 
         }
